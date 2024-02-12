6f4456dc9f73420f0636cea10dde22705e8db3c1
JocelynDeloose <jocelyn.deloose59@gmail.com>
2024-02-12 15:56:03 +0100

    First commit

---
+++ src/main/java/com/luxiergerie/LuxiergerieApplication.java
@@ -0,0 +1,13 @@
+package com.luxiergerie.luxiergerie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxiergerieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxiergerieApplication.class, args);
+	}
+
+}
