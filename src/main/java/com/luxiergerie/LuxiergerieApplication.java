b8cd0c49d6509e1e28879029b0cd75306e739124
Maxime <auneau.maxime@gmail.com>
2024-02-13 12:32:19 +0100

    fix

--- src/main/java/com/luxiergerie/LuxiergerieApplication.java
+++ src/main/java/com/luxiergerie/LuxiergerieApplication.java
@@ -1,4 +1,4 @@
-package com.luxiergerie.luxiergerie;
+package com.luxiergerie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
