77a13d1ef9c1928d8cc4607f8adf7a3721caa49d
mohamedehn <mohamed.ebarhmatin@yahoo.fr>
2024-02-19 10:28:14 +0100

    add getRolesByEmployee & fix nonNull error

--- src/main/java/com/luxiergerie/Domain/Config/WebHexagonalConfig.java
+++ src/main/java/com/luxiergerie/Domain/Config/WebHexagonalConfig.java
@@ -1,17 +1,22 @@
 package com.luxiergerie.Domain.Config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebHexagonalConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-                .allowedHeaders("Content-Type", "Date", "Total-Count", "loginInfo", "Authorization")
-                .allowCredentials(true);
+    public void addCorsMappings(@Nullable CorsRegistry registry) {
+      if (registry != null) {
+          registry.addMapping("/**")
+          .allowedOrigins("http://localhost:4200")
+          .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+          .allowedHeaders("Content-Type", "Date", "Total-Count", "loginInfo", "Authorization")
+          .allowCredentials(true);
+        } else {
+          throw new RuntimeException("CorsRegistry must not be null");
+        }
     }
 }
