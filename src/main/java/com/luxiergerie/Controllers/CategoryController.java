ede829207a38d6a7ead0abc7975da498124325a7
JocelynDeloose <jocelyn.deloose59@gmail.com>
2024-03-22 11:08:14 +0100

    create roleController + refacto CategoryController

--- src/main/java/com/luxiergerie/Controllers/CategoryController.java
+++ src/main/java/com/luxiergerie/Controllers/CategoryController.java
@@ -5,77 +5,75 @@ import com.luxiergerie.Domain.Entity.Category;
 import com.luxiergerie.Domain.Entity.Section;
 import com.luxiergerie.Domain.Repository.CategoryRepository;
 import com.luxiergerie.Domain.Repository.SectionRepository;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.web.bind.annotation.*;
+import static java.util.Objects.requireNonNull;
 
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
-  private final CategoryRepository categoryRepository;
-  private final SectionRepository sectionRepository;
+    private final CategoryRepository categoryRepository;
+    private final SectionRepository sectionRepository;
 
-  public CategoryController(CategoryRepository categoryRepository, SectionRepository sectionRepository) {
-    this.categoryRepository = categoryRepository;
-    this.sectionRepository = sectionRepository;
-  }
+    public CategoryController(CategoryRepository categoryRepository, SectionRepository sectionRepository) {
+        this.categoryRepository = categoryRepository;
+        this.sectionRepository = sectionRepository;
+    }
 
-  @GetMapping("/categories")
-  public List<Category> getAllCategories() {
-    return categoryRepository.findAll();
-  }
+    @GetMapping("/categories")
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
 
-  @GetMapping("/categories/{id}")
-  public Category getCategoryById(@PathVariable UUID id) {
-    UUID nonNullId = Objects.requireNonNull(id, "Category ID must not be null");
-    return this.categoryRepository.findById(nonNullId)
-        .orElseThrow(() -> new RuntimeException("Category not found with id: " + nonNullId));
-  }
+    @GetMapping("/categories/{id}")
+    public Category getCategoryById(@PathVariable UUID id) {
+        UUID nonNullId = requireNonNull(id, "Category ID must not be null");
+        return this.categoryRepository.findById(nonNullId)
+                .orElseThrow(() -> new RuntimeException("Category not found with id: " + nonNullId));
+    }
 
-  @GetMapping("/categories/{id}/accommodations")
-  public List<Accommodation> getAccommodationsByCategories(@PathVariable UUID id) {
-    Optional<Category> categoryOptional = categoryRepository.findById(id);
-    if (categoryOptional.isPresent()) {
-        Category category = categoryOptional.get();
-        return category.getAccommodations();
-    } else {
+    @GetMapping("/categories/{id}/accommodations")
+    public List<Accommodation> getAccommodationsByCategories(@PathVariable UUID id) {
+        Optional<Category> categoryOptional = categoryRepository.findById(id);
+        if (categoryOptional.isPresent()) {
+            Category category = categoryOptional.get();
+            return category.getAccommodations();
+        }
         throw new RuntimeException("Section not found with id: " + id);
     }
-  }
 
-  @PostMapping("/sections/{section_id}/categories")
-  public Category createCategory(@RequestBody Category category, @PathVariable UUID section_id) {
-    Optional<Section> sectionOptional = sectionRepository.findById(section_id);
-    if (sectionOptional.isPresent()) {
-        Section section = sectionOptional.get();
-        category.setSection(section);
-        return categoryRepository.save(category);
-    } else {
+    @PostMapping("/sections/{section_id}/categories")
+    public Category createCategory(@RequestBody Category category, @PathVariable UUID section_id) {
+        Optional<Section> sectionOptional = sectionRepository.findById(section_id);
+        if (sectionOptional.isPresent()) {
+            Section section = sectionOptional.get();
+            category.setSection(section);
+            return categoryRepository.save(category);
+        }
         throw new RuntimeException("Section not found with id: " + section_id);
     }
-  }
 
-  @PutMapping("/categories/{id}")
-  public Category updateCategory(@PathVariable UUID id, @RequestBody Category category) {
+    @PutMapping("/categories/{id}")
+    public Category updateCategory(@PathVariable UUID id, @RequestBody Category category) {
 
-      Optional<Category> categoryOptional = categoryRepository.findById(id);
-    if (categoryOptional.isPresent()) {
-        Category categoryToUpdate = categoryOptional.get();
-        categoryToUpdate.setName(category.getName());
-        categoryToUpdate.setImage(category.getImage());
-        categoryToUpdate.setDescription(category.getDescription());
-        return categoryRepository.save(categoryToUpdate);
-    } else {
+        Optional<Category> categoryOptional = categoryRepository.findById(id);
+        if (categoryOptional.isPresent()) {
+            Category categoryToUpdate = categoryOptional.get();
+            categoryToUpdate.setName(category.getName());
+            categoryToUpdate.setImage(category.getImage());
+            categoryToUpdate.setDescription(category.getDescription());
+            return categoryRepository.save(categoryToUpdate);
+        }
         throw new RuntimeException("Category not found with id: " + id);
     }
-  }
 
-  @DeleteMapping("/categories/{id}")
-  public void deleteCategory(@PathVariable UUID id) {
-    categoryRepository.deleteById(id);
-  }
+    @DeleteMapping("/categories/{id}")
+    public void deleteCategory(@PathVariable UUID id) {
+        categoryRepository.deleteById(id);
+    }
+
 }
---
+++ src/test/java/com/luxiergerie/Controllers/RoleControllerTest.java
@@ -0,0 +1,48 @@
+package com.luxiergerie.Controllers;
+
+import com.luxiergerie.Domain.Entity.Role;
+import com.luxiergerie.Domain.Repository.RoleRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class RoleControllerTest {
+
+    @InjectMocks
+    RoleController roleController;
+
+    @Mock
+    private RoleRepository roleRepository;
+
+    @Test
+    public void shouldAddANewRole_whenIUseAddANewRoleFromRoleController() {
+        // Arrange
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+
+        Role expectedRole = new Role(UUID.randomUUID(), "ROLE_EMPLOYEE", new ArrayList<>());
+        when(roleRepository.save(expectedRole)).thenReturn(expectedRole);
+
+        // Act
+        Role addedRole = roleController.addANewRole(expectedRole);
+
+        // Assert
+
+        assertThat(addedRole.getId()).isEqualTo(expectedRole.getId());
+        assertThat(addedRole.getName()).isEqualTo(expectedRole.getName());
+    }
+
+
+}
