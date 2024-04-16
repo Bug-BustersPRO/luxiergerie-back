b6eccba267f57479e725a2d7ce9964418299cd50
JocelynDeloose <jocelyn.deloose59@gmail.com>
2024-04-16 09:35:28 +0200

    rename dto by DTO

--- src/main/java/com/luxiergerie/DTO/PurchaseDTO.java
+++ src/main/java/com/luxiergerie/DTO/PurchaseDTO.java
@@ -1,61 +1,61 @@
 package com.luxiergerie.DTO;
 
+import com.luxiergerie.Domain.Entity.Accommodation;
+import com.luxiergerie.Domain.Entity.Room;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-import com.luxiergerie.Domain.Entity.Accommodation;
-import com.luxiergerie.Domain.Entity.Room;
 
 public class PurchaseDTO {
+  private UUID id;
+  private Date date;
+  private Room room;
+  private String status;
+  private List<Accommodation> accommodations = new ArrayList<>();
 
-    private UUID id;
-    private Date date;
-    private Room room;
-    private String status;
-    private List<Accommodation> accommodations;
+  public PurchaseDTO() {
+  }
 
-   public PurchaseDTO() {
-    }
+  public UUID getId() {
+    return id;
+  }
 
-    public UUID getId() {
-        return id;
-    }
+  public void setId(UUID id) {
+    this.id = id;
+  }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
+  public Date getDate() {
+    return date;
+  }
 
-    public Date getDate() {
-        return date;
-    }
+  public void setDate(Date date) {
+    this.date = date;
+  }
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
+  public Room getRoom() {
+    return room;
+  }
 
-    public Room getRoom() {
-        return room;
-    }
+  public void setRoom(Room room) {
+    this.room = room;
+  }
 
-    public void setRoom(Room room) {
-        this.room = room;
-    }
+  public String getStatus() {
+    return status;
+  }
 
-    public String getStatus() {
-        return status;
-    }
+  public void setStatus(String status) {
+    this.status = status;
+  }
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+  public List<Accommodation> getAccommodations() {
+    return accommodations;
+  }
 
-    public List<Accommodation> getAccommodations() {
-        return accommodations;
-    }
-
-    public void setAccommodations(List<Accommodation> accommodations) {
-        this.accommodations = accommodations;
-    }
+  public void setAccommodations(List<Accommodation> accommodations) {
+    this.accommodations = accommodations;
+  }
 
 }
