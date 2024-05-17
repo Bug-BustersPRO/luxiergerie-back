172cbf6fdfe6ace13d1cb0261264f360c57c2926
MaaxRR <69145653+maxime-auneau@users.noreply.github.com>
2024-05-17 16:25:19 +0200

    lux-72 (#32)
    PIN by sms and mail done

---
+++ src/main/java/com/luxiergerie/Services/EmailService.java
@@ -0,0 +1,46 @@
+package com.luxiergerie.Services;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Properties;
+
+@Service
+public class EmailService {
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    public void sendEmail(String to, String subject, String text) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "sandbox.smtp.mailtrap.io");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new jakarta.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("noreply@luxiergerie.com"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(subject);
+            message.setText(text);
+            Transport.send(message);
+            System.out.println("Email sent successfully");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/luxiergerie/Services/SMSService.java
@@ -0,0 +1,32 @@
+package com.luxiergerie.Services;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SMSService {
+
+    @Value("${twilio.account.sid}")
+    private String accountSid;
+
+    @Value("${twilio.auth.token}")
+    private String authToken;
+
+    @Value("${twilio.phone.number}")
+    private String twilioPhoneNumber;
+
+    public void sendSMS(String toPhoneNumber, String messageBody) {
+        Twilio.init(accountSid, authToken);
+
+        Message message = Message.creator(
+                        new PhoneNumber(toPhoneNumber),
+                        new PhoneNumber(twilioPhoneNumber),
+                        messageBody)
+                .create();
+
+        System.out.println("SMS sent successfully. Message SID: " + message.getSid());
+    }
+}
