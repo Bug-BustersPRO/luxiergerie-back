3eea5275389ad033145a1fcd677ecff47aae64c1
ehn <mohamed.ebarhmatin@yahoo.fr>
2024-05-17 16:53:44 +0200

    entity done

---
+++ src/main/java/com/luxiergerie/Domain/Entity/Hotel.java
@@ -0,0 +1,63 @@
+package com.luxiergerie.Domain.Entity;
+
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "hotel")
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, name = "name", length = 50)
+    private String name;
+
+    @Column(nullable = false, name = "image", length = 50)
+    private String image;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "hotel_colors", joinColumns = @JoinColumn(name = "hotel_id"))
+    @Column(nullable = false, name = "colors", length = 50)
+    private List<String> colors;
+
+    public Hotel() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public List<String> getColors() {
+        return colors;
+    }
+
+    public void setColors(List<String> colors) {
+        this.colors = colors;
+    }
+}
+
