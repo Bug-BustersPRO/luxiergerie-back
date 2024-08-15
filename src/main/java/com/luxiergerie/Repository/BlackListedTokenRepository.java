562d9d51d473ec2b300407b34627f45e140ea956
ehn <mohamed.ebarhmatin@yahoo.fr>
2024-08-15 14:48:31 +0200

    Changes by maxr

--- src/main/java/com/luxiergerie/Repository/BlackListedTokenRepository.java
+++ src/main/java/com/luxiergerie/Repository/BlackListedTokenRepository.java
@@ -2,9 +2,11 @@ package com.luxiergerie.Repository;
 
 import com.luxiergerie.Model.Entity.BlackListedToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface BlackListedTokenRepository extends JpaRepository<BlackListedToken, UUID> {
     public BlackListedToken findByUserIdAndIsBlackListed(UUID userId, boolean isBlackListed);
 
